45a8215099cbd6df455ca707a327a8ada53d34db
Piyush <apiyushgarg2006@gmail.com>
2025-08-03 11:07:46 +0530

    Fixed issue

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -175,10 +175,10 @@ const Footer = () => {
         <div className="mt-6 flex flex-col md:flex-row justify-between items-center gap-4 text-gray-600 dark:text-gray-400 ">
           {/* Social Icons */}
           <div className="flex space-x-6">
-            <a href="https://facebook.com">
+            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
               <Facebook className="w-5 h-5 hover:text-orange-500" />
             </a>
-            <a href="https://twitter.com">
+            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="w-5 h-5 hover:text-orange-500"
@@ -188,10 +188,10 @@ const Footer = () => {
                 <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
               </svg>
             </a>
-            <a href="https://linkedin.com">
+            <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
               <Linkedin className="w-5 h-5 hover:text-orange-500" />
             </a>
-            <a href="https://instagram.com">
+            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
               <Instagram className="w-5 h-5 hover:text-orange-500" />
             </a>
           </div>
