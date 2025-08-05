45045be15ea22fa75b21792ee2eb494132d32408
Sumit Gorai <goraisumit01@gmail.com>
2025-08-05 12:23:27 +0530

    update the contributors

--- src/components/contributors/contribution.js
+++ src/components/contributors/contribution.js
@@ -1,11 +1,28 @@
-const getContributors = async({perPage=30, page=1}) => {
-    try {
-        const data = await fetch(`https://api.github.com/repos/SumitGorai01/TechieBlog/contributors?per_page=${perPage}&page=${page}`);
-        return await data.json();
-    } catch (error) {
-        console.log(error);
-        return null
-    }
-}
+const getContributors = async () => {
+  try {
+    let allContributors = [];
+    let page = 1;
+    const perPage = 10; // GitHub max
+    let hasMore = true;
 
+    while (hasMore) {
+      const res = await fetch(
+        `https://api.github.com/repos/SumitGorai01/TechieBlog/contributors?per_page=${perPage}&page=${page}`
+      );
+      const contributors = await res.json();
+
+      if (contributors.length === 0) {
+        hasMore = false;
+      } else {
+        allContributors = [...allContributors, ...contributors];
+        page++;
+      }
+    }
+
+    return allContributors;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
 export { getContributors }
